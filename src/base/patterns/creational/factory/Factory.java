ca7d5d927e19a6b9065efe2227464e525b84dca2
Ilia Lenskii <il.lenskii@gmail.com>
2023-05-29 18:08:50 +0300

    Abstract factory

--- src/base/patterns/creational/factory/Factory.java
+++ src/base/patterns/creational/factory/Factory.java
@@ -7,13 +7,10 @@ package base.patterns.creational.factory;
 public class Factory {
     
     public static void main(String[] args) {
-        
-        ShooterGenerator takeShotBow = new BowGenerator();
-        
-        takeShotBow.takeShot();
-        
-        ShooterGenerator takeShotCross = new CrossbowGenerator();
-        
-        takeShotCross.takeShot();
+
+        ShooterGenerator takeWeapon = new CrossbowGenerator(); // BowGenerator
+        GameWeapon wg = takeWeapon.showWeapon();
+
+        wg.shot();
     }
 }
